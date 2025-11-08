e406f7080fc44eed2557290bab76fadc342b6691
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-07 23:46:16 -0300

    Add files via upload

---
+++ texto.py
@@ -0,0 +1,29 @@
+multiples = """
+Hola 
+mundo
+desde
+tucuman
+argentina
+"""
+
+print(multiples)
+
+palabra = "murcielago"
+print(len(palabra))
+
+texto = "este curso es de fundamentos de python"
+esta_incluida = 'python' in texto
+print(esta_incluida)
+
+no_esta_incluido = 'javascript' not in texto
+print(no_esta_incluido)
+
+mayuscula = texto.upper()
+print(mayuscula)
+minuscula = texto.lower()
+print(minuscula)
+
+espacios = "    Esto es un texto     "
+sin_espacios = espacios.strip()
+print(sin_espacios)
+
