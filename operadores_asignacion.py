cd7ccb4b77412c39ea315a6a8439febd248ebfd4
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-11 15:34:21 -0300

    Add files via upload

---
+++ operadores_asignacion.py
@@ -0,0 +1,20 @@
+#operadores de asignacion
+
+x=5
+
+x = x+3
+x += 3 #suma
+x -= 3 #resta
+x *= 3 #multiplicacion
+x /= 3 #division
+x %= 2 #modulo
+
+y = 20
+
+y //= 2 #division entera
+y **= 3 #exponente
+
+# WALRUS (morsa) :=
+
+print(z := 3) #asigna dentro de un metodo una variable
+print(z)
