014b1be0c081696603d486c2f845635a767f8c0d
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-11 15:25:19 -0300

    Add files via upload

---
+++ operadores.py
@@ -0,0 +1,28 @@
+#operadores aritmeticos
+
+x=5
+y=10
+
+#suma
+print("suma", x+y)
+#resta
+print("resta", x-y)
+#multiplocacion
+print("multiplicacion", x*y)
+#division
+print("division", y/x)
+#modulo %
+print("modulo", x%y)
+#potencia
+print("potencia", y**x)
+#division //
+print("division entera", y//x)
+
+#precedencia de operadores
+
+#parentesis
+#exponentes
+#multiplicaciones, divisiones, divisiones enteras y restos
+#sumas y restas
+#comparacion de identidad y pertenencia
+#logicos
