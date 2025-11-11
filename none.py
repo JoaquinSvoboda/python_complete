cec201f9c54a3faa9084d872fff11bcea15b0da1
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-10 23:16:22 -0300

    Add files via upload

---
+++ none.py
@@ -0,0 +1,8 @@
+x = None
+print(x)          
+print(type(x)) #NoneType
+
+print(type(""))   # String vacío.
+print(type(0))     # Número cero.
+print(type(False)) # Booleano false.
+print(type([]))    # Lista vacía.
