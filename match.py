bc859896b202b950cb341da8476d6ba54e5343e7
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-12 15:04:04 -0300

    Add files via upload

---
+++ match.py
@@ -0,0 +1,11 @@
+dia = 1
+
+match dia:
+    case 1:
+        print("hoy es lunes")
+    case 2:
+        print("hoy es martes")
+    case 3:
+        print("hoy es miercoes")
+    case _:
+        print("hoy es otro dia")
