259c25de8c310ace862f660bbf46cfb9c42fdae7
Joaquin <150185174+JoaquinSvoboda@users.noreply.github.com>
2025-11-15 14:21:15 -0300

    Add files via upload

---
+++ modulos_nuevo/main.py
@@ -0,0 +1,7 @@
+# main.py
+import operaciones
+
+print(operaciones.sumar(2, 3))       # 5
+print(operaciones.restar(2, 3))      # -1
+print(operaciones.multiplicar(2, 3)) # 6
+print(operaciones.dividir(10, 5))    # 2.0
---
+++ modulos_nuevo/operaciones.py
@@ -0,0 +1,13 @@
+# operaciones.py
+
+def sumar(A, B):
+    return A + B
+
+def restar(A, B):
+    return A - B
+
+def multiplicar(A, B):
+    return A * B
+
+def dividir(A, B):
+    return A / B
